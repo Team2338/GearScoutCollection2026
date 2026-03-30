6ad1186c2e0e1cb94950e354930acc631cc030b8
Ronan Ludvigsen <ronanludvigsen@gmail.com>
2026-03-30 14:09:22 -0500

    Refactor update banner to use modal overlay for improved user experience

--- src/components/update-banner/UpdateBanner.tsx
+++ src/components/update-banner/UpdateBanner.tsx
@@ -7,16 +7,21 @@ interface IProps {
 
 export default function UpdateBanner(props: IProps) {
 	return props.hasUpdate && props.serviceWorker && (
-		<div className="update-available-banner">
-			<span>An update is available!</span>
-			<button
-				className="update-button"
-				onClick={ () => {
-					props.serviceWorker!.postMessage('SKIP_WAITING');
-				}}
-			>
-				Update
-			</button>
+		<div className="update-modal-overlay">
+			<div className="update-modal-container">
+				<div className="update-modal-content">
+					<h1 className="update-modal-title">Update Required</h1>
+					<p className="update-modal-message">A critical update is available for GearScout. Please update to continue using the application.</p>
+					<button
+						className="update-modal-button"
+						onClick={ () => {
+							props.serviceWorker!.postMessage('SKIP_WAITING');
+						}}
+					>
+						Update Now
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
